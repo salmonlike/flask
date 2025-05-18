161b39e88388d654404e62e90f7a1de6b24b10ac
kanta <kanta@kantanoMacBook-Air.local>
2025-05-18 13:03:40 +0900

    Update HTML and CSS

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("index.html")  # ← templates/index.html を返す
